7ee4f5af0662fba2f3a4028ffe0e2b4f3538332a
fuyuanxiu <fuyuanxiu@126.com>
2020-08-24 16:52:28 +0800

    fyx-pull

--- js/api.js
+++ js/api.js
@@ -2,8 +2,9 @@
 var API = window.API = {
     //webPath			: "http://192.168.1.105:9999/",
 	webPathWai			: "http://113.106.220.62:9999/jyapp/",
-	//webPath			: "http://192.168.1.104:9999/",
+	//webPath			: "http://192.168.1.105:9998/",
 	webPath			: "http://192.168.1.160:9998/hl_rf",
+	//webPath			: "http://192.168.22.9:8080/hl_rf",
 
 	version         :"test",//demo:静态，prod:正式，test:测试
 	urls: {
